9c7e54f83a38fb5199d0968d770ee42783f8a8dc
gatelogic <49165306+gatelogic@users.noreply.github.com>
2021-11-27 16:41:40 +0000

    Add files via upload

---
+++ Integers/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 123;
+        long x1 = 1541526319851236156L;
+        byte x2 = 100;
+        double x3 = 45984.12859;
+        float x4 = 256.156f;
+        boolean x5 = true;
+        char x6 = 'f';
+        String x7 = "GateLogic!";
+
+        System.out.println(x);
+        System.out.println("My number is: "+x);
+        System.out.println(x1+" - this is a long data type");
+        System.out.println(x2+" - this is a byte data type");
+        System.out.println(x3+" - this is a double data type");
+        System.out.println(x4+" - this is a float data type");
+        System.out.println(x5+" - this is a boolean data type");
+        System.out.println(x6+" - this is a char data type");
+        System.out.println(x7+" - this is a string data type");
+        System.out.println("Hey "+x7);
+    }
+}
