b568a13b33e7241ac7f90b0af5c70ca7e61dfb10
gatelogic <49165306+gatelogic@users.noreply.github.com>
2021-12-05 17:10:33 +0000

    Add files via upload

---
+++ Swapping Variables/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+        String x = "water";
+        String y = "orange";
+        String temp;
+
+        temp=x;
+        x=y;
+        y=temp;
+
+        System.out.println("x: "+x);
+        System.out.println("y: "+y);
+    }
+}
